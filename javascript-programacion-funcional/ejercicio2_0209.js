d72b7d927a74c1b24f35d16d10fab10b0162668c
jerry <gerardo.cardona.ram@gmail.com>
2026-09-08 19:59:56 -0600

    feat: completados los ejercicios de JavaScript Funcional del dia 02-09-2026

---
+++ javascript-programacion-funcional/ejercicio2_0209.js
@@ -0,0 +1,6 @@
+
+// Funcion que eleva a la cuarta potencia un numero
+
+const potenciaNum = n => n**4;
+
+console.log(potenciaNum(4));
---
+++ javascript-programacion-funcional/ejercicio3_0209.js
@@ -0,0 +1,6 @@
+// funcion que calcula si un numero es mayor a 18
+
+const mayor18 = (n) => n > 18 ? "el numero es MAYOR a 18" : "el numero NO es mayor a 18";
+
+console.log(mayor18(19));
+console.log(mayor18(18));
